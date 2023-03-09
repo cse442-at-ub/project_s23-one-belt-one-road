de830672d03cb44b6445fa5ad2429eb4016a36bd
yigiaom <112595895+yigiaom@users.noreply.github.com>
2023-03-09 17:31:59 -0500

    paymentpage draft

---
+++ paymentpage.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment Page</title>
+</head>
+<body>
+<form>
+    <label for="address">Address:</label>
+    <input type="text" id="address" name="Address"><br><br>
+
+    <label for="fullname">Full Name:</label>
+    <input type="text" id="fullname" name="Full name"><br><br>
+
+    <label for="phone">Phone Number:</label>
+    <input type="text" id="phone" name="Phone number"><br><br>
+
+    <label for="city">City:</label>
+    <input type="text" id="city" name="City"><br><br>
+
+    <label for="state">State:</label>
+    <input type="text" id="state" name="State"><br><br>
+
+    <label for="zip">Zip Code:</label>
+    <input type="text" id="zip" name="Zip code"><br><br>
+
+    <label for="cardnumber">Card Number:</label>
+    <input type="text" id="cardnumber" name="Card number"><br><br>
+
+    <label for="nameoncard">Name on Card:</label>
+    <input type="text" id="nameoncard" name="Name on card"><br><br>
+
+    <label for="expiration">Expiration Date:</label>
+    <input type="text" id="expiration" name="Expiration date"><br><br>
+
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
