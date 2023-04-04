ea385e54a9a9a5efc931726c32ebefc6e0ca1b05
yigiaom <yixiaoma@buffalo.edu>
2023-04-03 23:46:08 -0400

    item listing page draft #52

---
+++ itemlisting.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Item Listing Page</title>
+	<link rel="stylesheet" href="itemlisting.css">
+</head>
+<body>
+    <header>
+		<?php require 'header.php'; ?>
+	</header>
+	<h1 style="color: #006ED3;">Item Listing Page</h1>
+
+	<div class="item-container">
+		<div class="item">
+			<a href="product_detail_page.php">
+				<img src="/images/hat1.jpg" alt="Hat 1">
+			</a>
+			<h3>Hat 1</h3>
+			<span>$10.00</span>
+		</div>
+
+		<div class="item">
+			<a href="product_detail_page.php">
+				<img src="/images/hat1.jpg" alt="Hat 2">
+			</a>
+			<h3>Hat 2</h3>
+			<span>$15.00</span>
+		</div>
+
+		<div class="item">
+			<a href="product_detail_page.php">
+				<img src="/images/hat1.jpg" alt="Hat 3">
+			</a>
+			<h3>Hat 3</h3>
+			<span>$20.00</span>
+		</div>
+        
+	</div>
+</body>
+</html>
