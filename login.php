64c44638be96631375f6a9ca0bf669eee2f25652
chrisStuhler <107435183+chrisStuhler@users.noreply.github.com>
2023-04-07 15:32:37 -0400

    refactor to use login API

--- login.php
+++ login.php
@@ -1,40 +1,12 @@
 <?php
+require_once 'apiFunctions.php';
 // Check if the form has been submitted
 $errors = [];
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	// Retrieve the username and password from the form data
 	$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
     $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-	if (empty($errors)) {
-		$hashed_password = password_hash($password, PASSWORD_DEFAULT);
-		$serverName = "oceanus.cse.buffalo.edu:3306";
-		$dbUser = "UBITNAME";
-		$dbPass = "PERSON NUM";
-		$dbName = "cse442_2023_spring_team_j_db";
-		$conn = mysqli_connect($serverName , $dbUser, $dbPass, $dbName);
-		if (!$conn) {
-			die("Connection failed: " . mysqli_connect_error());
-		}
-		$query = "SELECT password FROM user WHERE username = '$username'";
-		$result = mysqli_query($conn, $query);
-		if (mysqli_num_rows($result) == 1) {
-			$row = mysqli_fetch_assoc($result);
-			$hashed_password = $row['password'];
-
-			if (password_verify($password, $hashed_password)) {
-				$_SESSION['logged_in'] = true;
-				echo '<p style="color: green;">' . "Successful login!" . '</p>';
-				// header('Location: landing.php');
-			  }
-			else{
-			$error_msg = 'Invalid username or password. Please try again';
-				}
-		}
-		else {
-		$error_msg = 'Invalid username or password. Please try again';
-		}
-		$conn->close();
-}
+	$error_msg = login($username , $password);
 }
 ?>
 
