ebc9da8b88b3085661d0d04add72f75d280a066b
Tina <66841628+harptin@users.noreply.github.com>
2023-04-08 02:40:13 -0400

    Implemented rendering buttons #70

--- header.php
+++ header.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 <html>
     <body>
         <div>
 
             <div style="float: right; display: flex; align-items: center;">
-                <a href="account.php" class="text-button" style="font-size: 18px; margin-top: -4px;">My Account</a>
-                <a href="cart.php">
-                    <img src="images/cart.png" alt="Shopping Cart" class="image-button" width="30">
-                </a>
+            <?php
+                session_start(); // Start the session
+                if (isset($_SESSION['username'])) { // If user is logged in
+                    echo '
+                        <a href="account.php" class="text-button" style="font-size: 18px; margin-top: -4px;">My Account</a>
+                        <a href="cart.php">
+                            <img src="images/cart.png" alt="Shopping Cart" class="image-button" width="30">
+                        </a>';
+                } else { // If user is not logged in
+                    echo '<a href="login.php" class="text-button" style="font-size: 18px; margin-top: -4px;">Log In</a>';
+                }
+            ?>
             </div>
 
             <div style="float: left;">
