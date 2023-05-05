1d006a04755da1ecace679f36064f057ff55f82e
yigiaom <yixiaoma@buffalo.edu>
2023-05-04 23:07:09 -0400

    fixed refresh bug of confirm page #9

--- confirm.php
+++ confirm.php
@@ -12,42 +12,12 @@
 			<h1>Thank you for your order!</h1>
 			<h2>Shipping Address:</h2>
 			<?php
-				require_once 'database_APIs/apiFunctions.php';
 				$shipping = $_SESSION["shipping"];
+				$amount = $_SESSION["amount"]; 
 				echo '<p><label style="white-space: nowrap;">' . $shipping . '</label></p>';
-
-				$userID = $_SESSION['user_id'];
-				$cart_result = getUserCart($userID);
-				$total_amount = 0;
-				$description = array();
-
-				if ($cart_result == -1) {
-					echo "<p>Error: Failed to get shopping data from API</p>";
-					exit;
-				} else {
-					while ($row = mysqli_fetch_assoc($cart_result)) {
-						$productID = $row['productID'];
-						$amount = $row['amount'];
-						$unitPrice = $row['unitPrice'];
-						$total_cost = $amount * $unitPrice;
-						$total_amount += $total_cost;
-
-						// Add product to the transaction description
-						$description[] = array(
-							"productID" => $productID,
-							"quantity" => $amount
-						);
-					}
-				}
-				echo '<h3>Total Amount: $' . $total_amount . '</h3>';
-
-				$result = addTransaction($userID, $total_amount, json_encode($description), $shipping);
-
-				$clear = clearUserCart($userID);
-
+				echo '<h3>Total Amount: $' . $amount . '</h3>';
 			?>
-			
-			</div>
+	</div>
 
 		
 </body>
--- payment.php
+++ payment.php
@@ -223,7 +223,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		$shipping = $address . ", " . $city . ", " . $state . " " . $zipcode;
 		$_SESSION["shipping"] = $shipping; 
-		
+		$userID = $_SESSION['user_id'];
+		$cart_result = getUserCart($userID);
+		$total_amount = 0;
+		$total_amount = floatval($total_amount);
+		$description = array();
+
+		if ($cart_result == -1) {
+			echo "<p>Error: Failed to get shopping data from API</p>";
+			exit;
+		} else {
+			while ($row = mysqli_fetch_assoc($cart_result)) {
+				$productID = $row['productID'];
+				$amount = $row['amount'];
+				$unitPrice = $row['unitPrice'];
+				$total_cost = $amount * $unitPrice;
+				$total_amount += $total_cost;
+
+				// Add product to the transaction description
+				$description[] = array(
+					"productID" => $productID,
+					"quantity" => $amount
+				);
+			}
+		}
+		$_SESSION["amount"] = $total_amount; 
+		$result = addTransaction($userID, $total_amount, json_encode($description), $shipping);
+		$clear = clearUserCart($userID);
+
 		header("Location: confirm.php"); 
 		exit; }
 					
