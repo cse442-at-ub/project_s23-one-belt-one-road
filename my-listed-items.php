e8b49a4245497dc8f549e9d1547c90b3d2181e5b
grayedge <garygao922@gmail.com>
2023-05-05 00:33:51 -0400

    Updated listed item page and clean the useless files #105

--- my-listed-items.php
+++ my-listed-items.php
@@ -31,110 +31,20 @@ $user_id = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : -1;
 				    } else {
 						while ($listed_row = $listed_result->fetch_assoc()) {
 							$image_path = $image_location . $listed_row['image'];
-							$product_path = 'product_detail.php?productID=' . $listed_row['productID'];
+							$product_path = 'product_detail.php?productID=' . $listed_row['id'];
 							echo '<div id="cartItem" class="item-block-long">';
 
 								echo '<div class="item-block-long-info">';
-									echo '<a href="' . $product_path . '" class="centered-link"><img src="' . $image_path . '" alt="item" class="item-block-long-image">' . $listed_row['productName'] . '</a>';
-									echo '<span class="item-price" type="number">$ ' . $listed_row['unitPrice'] . '</span>';
+									echo '<a href="' . $product_path . '" class="centered-link"><img src="' . $image_path . '" alt="item" class="item-block-long-image">' . $listed_row['product_name'] . '</a>';
+									echo '<span class="item-price" type="number">$ ' . $listed_row['unit_price'] . '</span>';
 								echo '</div>';
 
-								echo '<input id="cartItem-amount-' . $listed_row['productID'] . '" type="number" class="item-amount" value="' . $listed_row['amount'] . '">';
-									// echo '<script>
-									// 	document.getElementById("cartItem-amount-' . $listed_row['productID'] . '").addEventListener("change", function() {
-									// 		calculateTotal()
-									// 		.then(() => {
-									// 			var userID = "' . $user_id . '";
-									// 		    var productId = "' . $listed_row['productID'] . '";
-									// 		    var amountChange = this.value - ' . $listed_row['amount'] . ';
-									// 		    updateCart(userID, productId, amountChange)
-									// 		    .then(() => location.reload(true))
-					        		// 			.catch(error => console.error(error.message));
-									// 		});
-									// 	});
-									// </script>';
-								echo '<span class="item-subtotal">Subtotal $ '. $listed_row['amount'] * $listed_row['unitPrice'] . '</span>';
-								echo '<button class="cart-remove-button" data-product-id="' . $listed_row['productID'] . '">REMOVE</button>';
+								echo '<span class="item-price" type="number">Inventory:  ' . $listed_row['inventory'] . '</span>';
 							echo '</div>';
 						}
 					}
 				?>
-
-				<!-- <script>
-
-				// Update an item amount in shopping cart
-				function updateCart(userID, productID, amountChange) {
-					return new Promise(function(resolve, reject) {
-						console.log("Passing parameters to XML", userID, productID, amountChange);
-					    var xhr = new XMLHttpRequest();
-					    xhr.onreadystatechange = function() {
-					        if (this.readyState == 4) {
-					        	if (this.status == 200) {
-					        		// Handle the response from the server
-						            var response = JSON.parse(this.responseText);
-						            console.log(response);
-						            if (response.success) {
-				                        resolve(response.message);
-				                    } else {
-				                        reject(new Error(response.message));
-				                    }
-					        	}
-					            else {
-						        	console.log(this.readyState, this.status);
-				                    reject(new Error("Failed to update item amount from cart"));
-				                }
-					        }
-					    }
-					    xhr.open("GET", "updateCart.php?userID=" + userID + "&productID=" + productID + "&amountChange=" + amountChange, true);
-					    xhr.send();
-					});
-				}
-
-				// Remove items from shopping cart
-				function removeFromCart(userID, productID) {
-				    // Send an AJAX request to the server-side PHP file
-				    return new Promise(function(resolve, reject) {
-					    var xmlhttp = new XMLHttpRequest();
-					    xmlhttp.onreadystatechange = function() {
-					        if (this.readyState == 4) {
-					        	if (this.status == 200) {
-					        		// Handle the response from the server
-						            var response = JSON.parse(this.responseText);
-						            console.log(response);
-						            if (response.success) {
-				                        resolve(response.message);
-				                    } else {
-				                        reject(new Error(response.message));
-				                    }
-					        	}
-					            else {
-						        	console.log(this.readyState, this.status);
-				                    reject(new Error("Failed to remove item from cart"));
-				                }
-					        } 
-					    };
-					    xmlhttp.open("GET", "removeFromCart.php?userID=" + userID + "&productID=" + productID, true);
-					    xmlhttp.send();
-					});
-				}
-
-				// Attach a click event listener to the "REMOVE" button
-				var removeButtons = document.getElementsByClassName("cart-remove-button");
-				for (var i = 0; i < removeButtons.length; i++) {
-				    removeButtons[i].addEventListener("click", function() {
-				        var userID = <?php echo $user_id; ?>;
-				        var productID = this.getAttribute("data-product-id");
-				        var amount = this.parentNode.querySelector(".item-amount").value;
-				        removeFromCart(userID, productID, amount)
-				        .then(() => location.reload(true))
-				        .catch(error => console.error(error.message));
-				    });
-				}
-				</script> -->
-
 			</div>
-			<!-- <label class="item-price">Total: $<span id="total"></span></label>
-			<a href="payment.php" class="blue-button" style="font-size: 20px; margin-left: 20px;">CHECKOUT</a> -->
 		</section>
 	</main>
 	<script src="functions.js"></script>
